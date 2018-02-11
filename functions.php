a17c8459ed7d225737cd53f289635094e9131ca4
Gijs Jorissen <mail@gijsjorissen.nl>
2018-02-11 23:18:52 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+// Block direct access
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+/*
+ * Allow automatic updates.
+ */
+add_filter('automatic_updates_is_vcs_checkout', '__return_false', 1);
+
+/**
+ * Require.
+ */
+foreach(['assets'] as $file) {
+    require get_stylesheet_directory()."/src/$file.php";
+}
---
+++ src/assets.php
@@ -0,0 +1,27 @@
+<?php
+
+// Block direct access
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Styles.
+ */
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('app', get_stylesheet_directory_uri().'/public/css/app.css', '', '', 'screen');
+});
+
+/**
+ * Scripts.
+ */
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_script('app', get_stylesheet_directory_uri().'/public/js/app.js', ['jquery'], '', true);
+
+    wp_localize_script('app', 'Trowel', [
+        'template_uri'   => get_template_directory_uri(),
+        'stylesheet_uri' => get_stylesheet_directory_uri(),
+        'home_url'       => get_home_url(),
+        'ajax_url'       => admin_url('admin-ajax.php')
+    ]);
+});
